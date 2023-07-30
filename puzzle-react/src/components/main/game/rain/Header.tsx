5701dbd9240a5b25166f17ef50e5df97988a1508
chebakovvolodymyr <chebakovvl@gmail.com>
2023-07-30 22:33:17 +0200

    Add dnd to first slide

--- puzzle-react/src/components/main/game/rain/Header.tsx
+++ puzzle-react/src/components/main/game/rain/Header.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames"
 import { FC } from "react"
 
 interface HeaderProps {
@@ -13,7 +14,9 @@ export const Header: FC<HeaderProps> = ({closeGame, isContinueButtonDisabled}) =
                     Lietaus iššūkis. Priskirk debesims jų tipų pavadinimus, pažymėk, iš kurių galima tikėtis lietaus ir spausk „Pateikti“.
                 </span>
             </div>
-            <button>Pateikti</button>
+            <button className={classNames({
+                'continue-button--disabled': isContinueButtonDisabled,
+            })}>Pateikti</button>
             <button className="question_close" onClick={closeGame}></button>
         </header>
     )
