776438984545e37937ab8f7ac2879adcfa373315
Katerina Chebakova <katyache2310@gmail.com>
2023-08-08 00:01:04 +0200

    sound

--- puzzle-react/src/components/main/game/Game.tsx
+++ puzzle-react/src/components/main/game/Game.tsx
@@ -9,7 +9,7 @@ interface GameProps {
 }
 
 export const Game: FC<GameProps> = ({correctFlippedCards}) => {
-    const [lastGame, setLastGame] = useState<Card | null>(cards[6])
+    const [lastGame, setLastGame] = useState<Card | null>(cards[7])
     const [isGameOver, setIsGameOver] = useState(false)
     
     useEffect(() => {
