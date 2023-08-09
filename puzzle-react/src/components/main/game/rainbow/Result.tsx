2bbcd90a233d4e683f88f2f517169865c7325536
Katerina Chebakova <katyache2310@gmail.com>
2023-08-09 23:54:49 +0200

    fixed

--- puzzle-react/src/components/main/game/rainbow/Result.tsx
+++ puzzle-react/src/components/main/game/rainbow/Result.tsx
@@ -2,22 +2,23 @@ export const Result = () => {
   return (
     <>
       <div className="rainbow-left-result">
-        Vaivorykštė – optinis atmosferos reiškinys, atsirandantis dėl šviesos lūžio, atspindžio ir sklaidos. Kai Saulė apšviečia įvairius atmosferoje esančius vandens lašelius – danguje pasimato ištisinė spalvų juosta. Vaivorykštės forma – spalvotas lankas, kurį sudaro ištisinis spektras: išorinis kraštas yra raudonas, už jo – oranžinis, geltonas, žalias, žydras, mėlynas ir violetinis. Dvigubos vaivorykštės atveju matomas antras, ne toks ryškus lankas, kurio spalvų linijos išsidėsčiusios atvirkštine tvarka.
+        <span>        Vaivorykštė – optinis atmosferos reiškinys, atsirandantis dėl šviesos lūžio, atspindžio ir sklaidos. Kai Saulė apšviečia įvairius atmosferoje esančius vandens lašelius – danguje pasimato ištisinė spalvų juosta. Vaivorykštės forma – spalvotas lankas, kurį sudaro ištisinis spektras: išorinis kraštas yra raudonas, už jo – oranžinis, geltonas, žalias, žydras, mėlynas ir violetinis. Dvigubos vaivorykštės atveju matomas antras, ne toks ryškus lankas, kurio spalvų linijos išsidėsčiusios atvirkštine tvarka.
 
-        Vaivorykštė niekada nebūna vienoje vietoje – jos matymo vieta priklauso nuo stebėtojo ir Saulės padėties, t. y. ji visada atsiranda priešingoje dangaus pusėje, nei Saulė.
+        <p>Vaivorykštė niekada nebūna vienoje vietoje – jos matymo vieta priklauso nuo stebėtojo ir Saulės padėties, t. y. ji visada atsiranda priešingoje dangaus pusėje, nei Saulė.</p></span>
+
       </div>
 
       <div className="rainbow-right-result">
         Kaip įsiminti vaivorykštės spalvas?
         Įsidėmėk šį sakinį – pirmosios žodžių raidės išduoda spalvų pavadinimus:
 
-        Raudonai,
-        o
-        gal
-        žaliai
-        žydi
-        marių
-        vandenai.
+        <span className="red">Raudonai,</span>
+        <span className="orange">o</span>
+        <span className="yellow">gal</span>
+        <span className="green">žaliai</span>
+        <span className="blue">žydi</span>
+        <span className="indigo">marių</span>
+        <span className="violet">vandenai.</span>
       </div>
     </>
   );
