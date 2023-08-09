d53ecac0484622f66f204aa3d67cb57fa739872b
Katerina Chebakova <katyache2310@gmail.com>
2023-08-09 22:31:16 +0200

    7 slide

--- puzzle-react/src/components/main/game/Game.tsx
+++ puzzle-react/src/components/main/game/Game.tsx
@@ -9,7 +9,7 @@ interface GameProps {
 }
 
 export const Game: FC<GameProps> = ({correctFlippedCards}) => {
-    const [lastGame, setLastGame] = useState<Card | null>(cards[7])
+    const [lastGame, setLastGame] = useState<Card | null>(cards[8])
     const [isGameOver, setIsGameOver] = useState(false)
     
     useEffect(() => {
