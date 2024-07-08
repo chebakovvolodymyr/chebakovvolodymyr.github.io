ebe55a7af6338cc18f4404c184cce5a3b1709730
a81734507 <a81734507@gmail.com>
2024-07-08 21:27:51 +0200

    marsrutai

---
+++ marsrutai/README.md
@@ -0,0 +1,5 @@
+node -v v20.10.0
+npm i
+npm i -g forever
+forever start app.js
+Open http://localhost:3000/
---
+++ marsrutai/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+  
+router.get('/map', (req, res) => {
+    res.render('map');
+});
+
+module.exports = router;
