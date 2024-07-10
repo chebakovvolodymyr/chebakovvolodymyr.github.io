a95722f5e380cf733d16d9d6e693f49c14e805af
Katerina Chebakova <katyache2310@gmail.com>
2024-07-10 23:19:43 +0200

    pages

--- marsrutai/routes/index.js
+++ marsrutai/routes/index.js
@@ -9,4 +9,16 @@ router.get('/map', (req, res) => {
     res.render('map');
 });
 
+router.get('/hike', (req, res) => {
+    res.render('hike');
+});
+
+router.get('/bicycle', (req, res) => {
+    res.render('bicycle');
+});
+
+router.get('/kayak', (req, res) => {
+    res.render('kayak');
+});
+
 module.exports = router;
