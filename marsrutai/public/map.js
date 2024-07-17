28ce3bb6a4e8572e3af84eeff560722335913f8d
Katerina Chebakova <katyache2310@gmail.com>
2024-07-17 23:02:20 +0200

    coords

---
+++ marsrutai/public/map.js
@@ -0,0 +1,106 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let scale = 1;
+    let lastScale = 1;
+    let translatePos = { x: 0, y: 0 };
+    let lastPos = { x: 0, y: 0 };
+    let doubleClickCount = 0;
+    const maxDoubleClickCount = 5;
+  
+    function initializeMap() {
+      const canvas = document.getElementById('mapCanvas');
+      const ctx = canvas.getContext('2d');
+      
+      canvas.width = window.innerWidth;
+      canvas.height = 1375;
+  
+      const img = new Image();
+      img.src = 'images/MAP.png';
+      img.onload = function() {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        addPoints(ctx);
+      };
+  
+      const hammertime = new Hammer(canvas);
+  
+      hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+      hammertime.get('pinch').set({ enable: true });
+
+      hammertime.on('panstart', function(event) {
+        lastPos = { x: translatePos.x, y: translatePos.y };
+      });
+  
+      hammertime.on('panmove', function(event) {
+        translatePos.x = lastPos.x + event.deltaX;
+        translatePos.y = lastPos.y + event.deltaY;
+        draw();
+      });
+  
+      hammertime.on('pinchstart', function(e) {
+        lastScale = scale;
+      });
+  
+      hammertime.on('pinch', function(e) {
+        scale = Math.max(0.5, Math.min(lastScale * e.scale, 3));
+        ctx.setTransform(scale, 0, 0, scale, 0, 0);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        addPoints(ctx);
+      });
+
+      hammertime.on('doubletap', function(event) {
+        handleDoubleClick(event);
+      });
+  
+      function draw() {
+        ctx.save();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.translate(translatePos.x, translatePos.y);
+        ctx.scale(scale, scale);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        addPoints(ctx);
+        ctx.restore();
+      }
+    
+      function handleDoubleClick(event) {
+        doubleClickCount += 1;
+        if (doubleClickCount > maxDoubleClickCount) {
+          scale = 1;
+          translatePos = { x: 0, y: 0 };
+          doubleClickCount = 0;
+        } else {
+          const canvasCenterX = canvas.width / 2;
+          const canvasCenterY = canvas.height / 2;
+      
+          const tapX = (event.center.x - canvas.offsetLeft - translatePos.x) / scale;
+          const tapY = (event.center.y - canvas.offsetTop - translatePos.y) / scale;
+      
+          scale *= 2;
+      
+          translatePos.x = canvasCenterX - tapX * scale;
+          translatePos.y = canvasCenterY - tapY * scale;
+      
+        }
+    
+    
+        draw();
+      }
+    }
+  
+    function addPoints(ctx) {
+      const pointsForRwnder = Object.values(points);
+  
+      ctx.fillStyle = 'red';
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 2;
+  
+      pointsForRwnder.forEach(point => {
+        ctx.beginPath();
+        ctx.arc(point.x, point.y, 5, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.stroke();
+      });
+    }
+  
+    initializeMap();
+  });
+  
--- marsrutai/routes/index.js
+++ marsrutai/routes/index.js
@@ -1,17 +1,46 @@
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 
+const language = {
+    LT: 'lt',
+    EN: 'en'
+}
+
 router.get('/', (req, res) => {
-    res.render('index', {language: 'lt'});
+    res.render('index', {language: language.LT});
 });
 
 router.get('/en', (req, res) => {
-    res.render('index', {language: 'en'});
+    res.render('index', {language: language.EN});
 });
   
 router.get('/map', (req, res) => {
-    res.render('map');
+    fs.readFile('./data/points.json',
+        { encoding: 'utf8', flag: 'r' },
+        function (err, data) {
+            if (err) {
+                console.log(err);
+                res.render('map', {language: language.LT, points: []});
+            } else {
+                res.render('map', {language: language.LT, points: JSON.parse(data)});
+            }
+        });
 });
+
+router.get('/map/en', (req, res) => {
+    fs.readFile('./data/points.json',
+        { encoding: 'utf8', flag: 'r' },
+        function (err, data) {
+            if (err) {
+                console.log(err);
+                res.render('map', {language: language.EN, points: []});
+            } else {
+                res.render('map', {language: language.EN, points: JSON.parse(data)});
+            }
+        });
+});
+
 
 router.get('/hike', (req, res) => {
     res.render('hike');
